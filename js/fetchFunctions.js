85a97711044719702b0f96377cab6d2bfd4ac9bd
Daniel Bach/Ofen <danielbach56@web.de>
2017-07-25 12:38:42 +0200

    Server verbessert, letzte Testphase startet jetzt.

--- js/fetchFunctions.js
+++ js/fetchFunctions.js
@@ -4,12 +4,12 @@ Diese Datei enthält alle Funktionen zur Datenanfrage beim Server (über HTTP-GE
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;		// XMLHttpRequest für GET-Abfragen einbinden
 
-// Funktion liefert ein Promise auf die Trackliste zurück
+// Funktion liefert ein Promise auf die Namen aller Tracks zurück
 module.exports.fetchTracklist = function () {
 	return new Promise(function (resolve, reject) {
 		// XMLhttpRequest fordert Ressource /tracks/:id an
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "http://localhost:8080/tracks", true);	// asynchrone GET-Anfrage
+		xhr.open("GET", "http://localhost:8080/tracks/names", true);	// asynchrone GET-Anfrage
 		xhr.addEventListener("error", error => { console.log(error.toString()); });
 		xhr.addEventListener("load", () => {
 			if (xhr.status >= 200 && xhr.status < 300) {
@@ -24,7 +24,7 @@ module.exports.fetchTracklist = function () {
 	});
 };
 
-// fetchTrack lädt asynchron die gewünschte .json Ressource (=trackId)
+// fetchTrack lädt asynchron eine konkrete .json Ressource (=trackId)
 // über eine GET-Anfrage und liefert ein Promise-Objekt zurück. Auf diesem wird in
 // 'showCoordinates()' auf das Ergebnis der Anfrage (.json Dokument) gewartet
 module.exports.fetchTrack = function (trackId) {
--- js/server.js
+++ js/server.js
@@ -8,6 +8,10 @@ const path = require("path");
 const fs = require("fs");	// zum auslesen des /assets/data Ordners
 var server = express();		// Express-Server Objekt wird zurückgeliefert
 
+// Asset-Ordner mit Trackdaten auslesen
+var files = fs.readdirSync("./assets/data");	// synchroner Lese-Zugriff auf die Namen der .json Dateien
+var fileCount = files.length;					// Anzahl der .json Dateien bestimmen
+
 // Kommandozeilenargument (port) einlesen und
 // BASE_URI anlegen
 const PORT = process.argv[2];
@@ -19,13 +23,31 @@ server.use(express.static(path.join(__dirname, "../release")));
 // Server starten
 server.listen(PORT, () => console.log(`Server is listening on ${BASE_URI}`));
 
-// /tracks liefert die Namen der Tracks in sortierter
-// Reihenfolge an den Browser aus
-server.get("/tracks", function (req, res) {
-	// Anzahl der Tracks im /assets/data Ordner bestimmen
-	var files = fs.readdirSync("./assets/data");	// synchroner Zugriff aufs Filesystem
-	var fileCount = files.length;
+/*
+FUNKTIONEN ZUR INTERAKTION MIT SERVER
+*/
 
+// /tracks liefert die Liste aller Tracks zurück
+server.get("/tracks", function (req, res) {
+	var tracks = [];
+	for (var i = 1; i <= fileCount; i++) {
+		// Alle Tracks requiren in Liste zusammenfassen
+		tracks.push(require("../assets/data/" + i + ".json"));
+	}
+
+	// Liste aller Tracks ausliefern
+	// Format: {<index> : <trackobjekt>}
+	// ACHTUNG: index wird hier ab 0 gezählt, weil 'names' eine Liste ist.
+	// Bei einer entsprechenden /tracks/:id Anfrage wird die angegebene id des Clients
+	// um 1 inkrementiert, da die .json Dateien im Fileystem ab index 1
+	// durchnummeriert sind.
+	res.json(tracks);
+	res.end();
+});
+
+// /tracks/names liefert die Namen der Tracks in sortierter
+// Reihenfolge an den Browser aus
+server.get("/tracks/names", function (req, res) {
 	// Über .json Files iterieren, Tracknamen herauslesen und abspeichern
 	var names = [];
 	for (var i = 1; i <= fileCount; i++) {
@@ -33,13 +55,6 @@ server.get("/tracks", function (req, res) {
 		var name = jsonFile.features[0].properties.name;
 		names.push(name);
 	}
-
-	// Namen als .json Datei an Browser ausliefern
-	// Format: {<index> : <name>}
-	// ACHTUNG: index wird hier ab 0 gezählt, weil 'names' eine Liste ist.
-	// Bei einer entsprechenden /tracks/:id Anfrage wird die angegebene id des Clients
-	// um 1 inkrementiert, da die .json Dateien im Fileystem ab index 1
-	// durchnummeriert sind.
 	res.json(names);
 	res.end();
 });
@@ -51,7 +66,14 @@ server.get("/tracks/:id", function (req, res) {
 	var id = req.params.id;
 	id++;
 
-	// Track anhand der id unterscheiden und jeweilige .json zurückliefern
-	res.json(require("./../assets/data/" + id + ".json"));
+	// Existiert ein Track mit dieser id?
+	if (id < 1 || id > fileCount) {
+		// Falls nicht => 'not found' an Client schicken
+		res.sendStatus(404);
+	}
+	else {
+		// sonst: Jeweilige :id.json zurückliefern
+		res.json(require("../assets/data/" + id + ".json"));
+	}
 	res.end();
 });
